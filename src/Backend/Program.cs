368819a40b8682726346cd7590958e2ef974af1e
A1exandr2 <skuklin546@gmail.com>
2025-09-19 00:30:10 +0300

    feat: test CI/CD

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -4,6 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "CI/CD рфботает, все получилось!");
+app.MapGet("/", () => "CI/CD рфботает, все получилось! Да и сегодня не спим походу");
 
 app.Run();
