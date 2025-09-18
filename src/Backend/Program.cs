064874bf3f56ab0b30b61c118944594d87b50bf2
A1exandr2 <skuklin546@gmail.com>
2025-09-18 22:42:23 +0300

    feat: update welcome message to test CI/CD.

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -4,6 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "CI/CD работает! Сервис обновлён автоматически!");
+app.MapGet("/", () => "CI/CD наверно работает! Сервис обновлён автоматически!");
 
 app.Run();
